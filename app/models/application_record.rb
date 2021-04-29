5cbc8297927cee651be5d286c09f4848078cf6bd
Clivern <hello@clivern.com>
2021-04-29 20:53:55 +0200

    init

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,13 @@
+# frozen_string_literal: true
+
+#
+# Pilgrim - A Lightweight And Powerful Control Panel, Set up in Minutes.
+#
+# Copyright 2020 Clivern. All rights reserved.
+# Use of this source code is governed by the MIT
+# license that can be found in the LICENSE file.
+#
+
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 end
